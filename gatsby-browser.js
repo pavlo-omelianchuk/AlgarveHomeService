d012688f7098f3726ad4b12936a621ae6544b69c
Pavlo Omelianchuk <danhipsalgarve@gmail.com>
2021-06-28 23:23:41 +0100

    Small Fixes

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,10 @@
 // ES5 way
 // exports.onClientEntry = () => {
 // ES6 way
-const onClientEntry = () => {
+export const onClientEntry = () => {
   // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
   if (!(`IntersectionObserver` in window)) {
     import(`intersection-observer`);
     console.log(`# IntersectionObserver is polyfilled!`);
   }
 };
-
-export default onClientEntry;
