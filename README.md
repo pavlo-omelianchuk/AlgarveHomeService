d8b8ac460b559f9620595279929cbdaa616147ca
Pavlo Omelianchuk <danhipsalgarve@gmail.com>
2021-06-29 22:32:30 +0100

    No Strapi

--- README.md
+++ README.md
@@ -27,3 +27,47 @@ View GraphiQL, an in-browser IDE, to explore your site's data and schema
 ⠀
 Local: http://localhost:8000/**_graphql
 On Your Network: http://172.26.208.1:8000/_**graphql
+
+{
+/_ <StaticQuery
+query={graphql` query MyQuery { strapiHero { MainPhoto { formats { large { url } } } Title Subtitle TitleName Button } } `}
+render={(data) => (
+<>
+<div>
+<section id="hero" className="jumbotron">
+<Container>
+<div id="hero-overlay" />
+{/_ <img src={'http://localhost:1337' + data.strapiHero.MainPhoto.formats.large.url} alt="hero-background-image" />
+<Fade big duration={4000}>
+<img src={pinkComponent} alt="pink component" />
+</Fade>
+<Fade bottom distance="30px">
+<h3 className="hero-subtitle">{data.strapiHero.Subtitle}</h3>
+</Fade>
+<Fade bottom duration={4000} distance="300px">
+<h1 className="hero-title">{data.strapiHero.Title}</h1>
+<h2 className="hero-title hero-title-name text-color-main">
+{` ${data.strapiHero.TitleName}`}
+</h2>
+</Fade>
+<Zoom duration={1000} delay={2000}>
+<p className="hero-cta">
+<span className="cta-btn cta-btn--hero">
+<a href="/prices">
+{data.strapiHero.Button}
+{/_ <a href="https://api.whatsapp.com/send?phone=+351 925 142 109">
+<i className="fa fa-whatsapp"
+                          ariaHidden="true"
+                        ></i>
+</a>
+</a>
+</span>
+</p>
+</Zoom>
+</Container>
+</section>
+</div>
+</>
+)}
+/> _/
+}
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,67 +1,45 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import { Fade, Zoom } from 'react-reveal';
-import { StaticQuery, graphql } from 'gatsby';
+// import { StaticQuery, graphql } from 'gatsby';
 import pinkComponent from '../../images/component2-pink.png';
 
 const Hero = () => (
-  <StaticQuery
-    query={graphql`
-      query MyQuery {
-        strapiHero {
-          MainPhoto {
-            formats {
-              large {
-                url
-              }
-            }
-          }
-          Title
-          Subtitle
-          TitleName
-          Button
-        }
-      }
-    `}
-    render={(data) => (
-      <>
-        <div>
-          <section id="hero" className="jumbotron">
-            <Container>
-              <div id="hero-overlay" />
-              {/* <img src={'http://localhost:1337' + data.strapiHero.MainPhoto.formats.large.url} alt="hero-background-image" /> */}
-              <Fade big duration={4000}>
-                <img src={pinkComponent} alt="pink component" />
-              </Fade>
-              <Fade bottom distance="30px">
-                <h3 className="hero-subtitle">{data.strapiHero.Subtitle}</h3>
-              </Fade>
-              <Fade bottom duration={4000} distance="300px">
-                <h1 className="hero-title">{data.strapiHero.Title}</h1>
-                <h2 className="hero-title hero-title-name text-color-main">
-                  {` ${data.strapiHero.TitleName}`}
-                </h2>
-              </Fade>
-              <Zoom duration={1000} delay={2000}>
-                <p className="hero-cta">
-                  <span className="cta-btn cta-btn--hero">
-                    <a href="/prices">
-                      {data.strapiHero.Button}
-                      {/* <a href="https://api.whatsapp.com/send?phone=+351 925 142 109">
+  <>
+    <div>
+      <section id="hero" className="jumbotron">
+        <Container>
+          <div id="hero-overlay" />
+
+          <Fade big duration={4000}>
+            <img src={pinkComponent} alt="pink component" />
+          </Fade>
+          <Fade bottom distance="30px">
+            <h3 className="hero-subtitle">Massage & Beauty therapy</h3>
+          </Fade>
+          <Fade bottom duration={4000} distance="300px">
+            <h1 className="hero-title">Home massage from</h1>
+            <h2 className="hero-title hero-title-name text-color-main">{` Danylo Hips`}</h2>
+          </Fade>
+
+          <Zoom duration={1000} delay={2000}>
+            <p className="hero-cta">
+              <span className="cta-btn cta-btn--hero">
+                <a href="/prices">
+                  make an appointment
+                  {/* <a href="https://api.whatsapp.com/send?phone=+351 925 142 109">
                         <i className="fa fa-whatsapp"
                           ariaHidden="true"
                         ></i>
-                      </a> */}
-                    </a>
-                  </span>
-                </p>
-              </Zoom>
-            </Container>
-          </section>
-        </div>
-      </>
-    )}
-  />
+                      </a>  */}
+                </a>
+              </span>
+            </p>
+          </Zoom>
+        </Container>
+      </section>
+    </div>
+  </>
 );
 
 export default Hero;
