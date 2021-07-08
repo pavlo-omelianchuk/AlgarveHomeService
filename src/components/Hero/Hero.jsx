dad49b5e0529f9d0325f13cf457eb160a9591855
Pavlo Omelianchuk <danhipsalgarve@gmail.com>
2021-07-08 23:32:28 +0100

    Restyling Header and Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -15,24 +15,17 @@ const Hero = () => (
             <img src={pinkComponent} alt="pink component" />
           </Fade>
           <Fade bottom distance="30px">
-            <h3 className="hero-subtitle">Massage & Beauty therapy</h3>
+            <h3 className="hero-overtitle">Massage & Beauty therapy</h3>
           </Fade>
           <Fade bottom duration={4000} distance="300px">
-            <h1 className="hero-title">Home massage from</h1>
+            <h1 className="hero-title">Massage at home from</h1>
             <h2 className="hero-title hero-title-name text-color-main">{` Danylo Hips`}</h2>
           </Fade>
 
           <Zoom duration={1000} delay={2000}>
             <p className="hero-cta">
               <span className="cta-btn cta-btn--hero">
-                <a href="/prices">
-                  make an appointment
-                  {/* <a href="https://api.whatsapp.com/send?phone=+351 925 142 109">
-                        <i className="fa fa-whatsapp"
-                          ariaHidden="true"
-                        ></i>
-                      </a>  */}
-                </a>
+                <a href="/prices">make an appointment</a>
               </span>
             </p>
           </Zoom>
