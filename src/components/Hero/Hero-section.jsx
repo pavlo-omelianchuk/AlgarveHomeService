3e912b0ef297130bfa62b5bc69bf629cc50fe649
Pavlo Omelianchuk <danhipsalgarve@gmail.com>
2021-07-22 19:57:41 +0100

    Hero Video

--- src/components/Hero/Hero-section.jsx
+++ src/components/Hero/Hero-section.jsx
@@ -1,67 +1,48 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Container, Row } from 'react-bootstrap';
 import StyledBackground from '../Image/HeroImg';
 import Hero from './Hero-component';
 import VideoBckg from '../Experiments/Experiments';
 
-const HeroSection = () => {
-  const HeroDesktop = () => {
-    <Container fluid>
-      <Row style={{ position: 'relative' }}>
-        <div id="home-desktop">
-          <VideoBckg>
-            <Hero />
-          </VideoBckg>
-        </div>
-      </Row>
-    </Container>;
-  };
-  const HeroMobile = () => {
-    <Container fluid>
-      <Row style={{ position: 'relative' }}>
-        <StyledBackground
-          className="hero-bg"
-          filename="bgHeroWithOpacity.jpg"
-          alt="Woman getting massage"
-          bgContent={<Hero />}
-        />
-      </Row>
-    </Container>;
-  };
+const HeroDesktop = () => {
+  return (
+    <div className="desktop">
+      <Container fluid>
+        <Row style={{ position: 'relative' }}>
+          <div id="home-desktop">
+            <VideoBckg>
+              <Hero />
+            </VideoBckg>
+          </div>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+const HeroMobile = () => {
+  return (
+    <div className="mobile">
+      <Container fluid>
+        <Row style={{ position: 'relative' }}>
+          <StyledBackground
+            className="hero-bg"
+            filename="bgHeroWithOpacity.jpg"
+            alt="Woman getting massage"
+            bgContent={<Hero />}
+          />
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
-  useEffect(() => {
-    if (window.innerWidth > 769) {
-      return <HeroDesktop />;
-    } else {
-      return <HeroMobile />;
-    }
-  }, []);
-  // if (window.innerWidth > 769) {
-  //   return (
-  //     <Container fluid>
-  //       <Row style={{ position: 'relative' }}>
-  //         <div id="home-desktop">
-  //           <VideoBckg>
-  //             <Hero />
-  //           </VideoBckg>
-  //         </div>
-  //       </Row>
-  //     </Container>
-  //   );
-  // } else {
-  //   return (
-  // <Container fluid>
-  //   <Row style={{ position: 'relative' }}>
-  //     <StyledBackground
-  //       className="hero-bg"
-  //       filename="bgHeroWithOpacity.jpg"
-  //       alt="Woman getting massage"
-  //       bgContent={<Hero />}
-  //     />
-  //   </Row>
-  // </Container>
-  //   );
-  // }
+const HeroSection = () => {
+  return (
+    <>
+      <HeroDesktop />
+      <HeroMobile />
+    </>
+  );
 };
 
 export default HeroSection;
