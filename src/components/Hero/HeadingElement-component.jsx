0d4cfb6cdc93890e93b5a8270541b8b9a54c6356
Pavlo Omelianchuk <danhipsalgarve@gmail.com>
2021-08-16 13:14:42 +0100

    Work on SEO and new post

--- src/components/Hero/HeadingElement-component.jsx
+++ src/components/Hero/HeadingElement-component.jsx
@@ -1,59 +1,107 @@
 import React from 'react';
 
-export default () => {
-  return (
-    <div style={{ position: 'absolute', color: 'transparent' }}>
-      <h1>Professional massage at Home over all the Algarve.</h1>
-      <h2>
-        {' '}
-        In Lagos, Albufeira, Quarteira, Vale de Lobo, Vilamoura, Armação de Pera, Portimao, Sagres
-      </h2>
-      <h3>We offer:</h3>
-      <h4>Health Massage</h4>
-      <h4>Golf Massage</h4>
-      <h4>Anti-Cellulite Massage</h4>
-      <h4>Therapeutic Massage</h4>
-      <h4>Sports Deep Tissue Massage</h4>
-      <h4>Relax Massage</h4>
-      <h4>Massage for Couples</h4>
-      <h4>Reflexology Massage</h4>
-      <h4>Anti-Cellulite Massage</h4>
-      <p>
-        We provide Professional massage at Home over all the Algarve. Every time you are on holidays
-        in warm Algarve, Portugal. We will be happy to visit you with best Massage Therapists for
-        Beauticians. But for now, you can read about Therapeutic Massage vs Deep Tissue. Therapeutic massage is a general term that
-        describes any type of massage modality that helps relieve pain, reduce stress, and work on a
-        specific problem—such as a frozen shoulder. People tend to assume therapeutic massage means
-        deep tissue massage, and that they will get a very strong massage. But this is not always
-        the case. Deep tissue massage is great at breaking down scar tissue and getting your blood
-        circulating, but it may not be what you need on a particular day. A good therapist will
-        apply the specific techniques and the right amount of pressure to address your need, and
-        sometimes that’s a lighter touch. Types of Therapeutic Massage There are several different
-        types of therapeutic massage in addition to deep tissue massage, such as: neuromuscular
-        massage myofascial massage trigger point therapy sports massage Many therapists will use a
-        combination of techniques depending on what your body needs. In addition, therapists will
-        often take additional classes in different techniques. You can see our therapists
-        specialties by clicking their bios. Benefits of Deep Tissue Massage Most of our clients are
-        interested in deep tissue massage, and that makes sense because it is a great way to manage
-        pain. Deep tissue massage—as well as the other therapeutic massage methods—can give you a
-        lot of relief from chronic pain. Many of our clients come in for frozen shoulder treatment
-        and neck pain—two side effects of working on computers all day. Deep Tissue Side Effects
-        Many people want to know what the side effects of deep tissue massage are. That’s a great
-        question, and it varies from person to person—and it can even vary for you depending on
-        what’s going on in your body the day you get a massage. Typically, people may feel more
-        muscle soreness for a day or two after the massage, especially if the therapist gives you a
-        strong massage. A deep tissue massage shouldn’t cause horrible pain—and if it does you need
-        to tell your therapist you need less pressure. However, if they’ve been working your
-        muscles, you will probably feel it as the toxins leave your body. Another side effect is a
-        feeling of disorientation when the massage is over (we call it massage drunk). You may be a
-        little light headed and out of it. That’s very normal. Just sit in the lobby and drink some
-        water until the feeling passes! You’ll usually be fine in 10 to 15 minutes. Finally, a deep
-        tissue massage will leave you dehydrated. That’s why your therapist gives you water when
-        it’s over. It is very important to drink plenty of water after a therapeutic massage. Where
-        to Get Deep Tissue Massage in Algarve. Our company, specialize in deep tissue massage. We
-        offer deep tissue massage in Sagres, Lagos, Portimão, Albufeira, Quarteira, Faro, Olhão,
-        Tavira and over all the Algarve. Our contacts: info.ahsmb@gmail.com+351 963 531 684
-      </p>
-    </div>
-  );
-};
+export default () => (
+  <div style={{ position: 'absolute', color: 'transparent' }}>
+    <h1>Professional massage at Home over all the Algarve.</h1>
+    <h2>
+      {' '}
+      In Lagos, Albufeira, Quarteira, Vale de Lobo, Vilamoura, Armação de Pera, Portimao, Sagres
+    </h2>
+    <h3>We offer:</h3>
+    <h4>Health Massage</h4>
+    <h4>Golf Massage</h4>
+    <h4>Anti-Cellulite Massage</h4>
+    <h4>Therapeutic Massage</h4>
+    <h4>Sports Deep Tissue Massage</h4>
+    <h4>Relax Massage</h4>
+    <h4>Massage for Couples</h4>
+    <h4>Reflexology Massage</h4>
+    <h4>Anti-Cellulite Massage</h4>
+    <p>
+      We provide Professional massage at Home over all the Algarve. Every time you are on holidays
+      in warm Algarve, Portugal. We will be happy to visit you with best Massage Therapists for
+      Beauticians. But for now, you can read about Therapeutic Massage vs Deep Tissue. Therapeutic
+      massage is a general term that describes any type of massage modality that helps relieve pain,
+      reduce stress, and work on a specific problem—such as a frozen shoulder. People tend to assume
+      therapeutic massage means deep tissue massage, and that they will get a very strong massage.
+      But this is not always the case. Deep tissue massage is great at breaking down scar tissue and
+      getting your blood circulating, but it may not be what you need on a particular day. A good
+      therapist will apply the specific techniques and the right amount of pressure to address your
+      need, and sometimes that’s a lighter touch. Types of Therapeutic Massage There are several
+      different types of therapeutic massage in addition to deep tissue massage, such as:
+      neuromuscular massage myofascial massage trigger point therapy sports massage Many therapists
+      will use a combination of techniques depending on what your body needs. In addition,
+      therapists will often take additional classes in different techniques. You can see our
+      therapists specialties by clicking their bios. Benefits of Deep Tissue Massage Most of our
+      clients are interested in deep tissue massage, and that makes sense because it is a great way
+      to manage pain. Deep tissue massage—as well as the other therapeutic massage methods—can give
+      you a lot of relief from chronic pain. Many of our clients come in for frozen shoulder
+      treatment and neck pain—two side effects of working on computers all day. Deep Tissue Side
+      Effects Many people want to know what the side effects of deep tissue massage are. That’s a
+      great question, and it varies from person to person—and it can even vary for you depending on
+      what’s going on in your body the day you get a massage. Typically, people may feel more muscle
+      soreness for a day or two after the massage, especially if the therapist gives you a strong
+      massage. A deep tissue massage shouldn’t cause horrible pain—and if it does you need to tell
+      your therapist you need less pressure. However, if they’ve been working your muscles, you will
+      probably feel it as the toxins leave your body. Another side effect is a feeling of
+      disorientation when the massage is over (we call it massage drunk). You may be a little light
+      headed and out of it. That’s very normal. Just sit in the lobby and drink some water until the
+      feeling passes! You’ll usually be fine in 10 to 15 minutes. Finally, a deep tissue massage
+      will leave you dehydrated. That’s why your therapist gives you water when it’s over. It is
+      very important to drink plenty of water after a therapeutic massage. Where to Get Deep Tissue
+      Massage in Algarve. Our company, specialize in deep tissue massage, sports deep tissue
+      massage. We offer deep tissue massage in Sagres, Lagos, Portimão, Albufeira, Quarteira, Faro,
+      Olhão, Tavira and over all the Algarve. Our contacts: info.ahsmb@gmail.com+351 963 531 684
+    </p>
+    <p>
+      BENEFITS OF GOLF MASSAGE Prevents Injuries. In golf, as with any other sport, there’s always a
+      chance for injury. Fortunately, massage is a great way to lower your risk. Regular sessions
+      can help reduce spasms and scar tissue and relieve trigger points. When your muscles are
+      relaxed, they are less likely to suffer strains or tears while playing. In addition, your
+      Tampa Bay Sports Massage therapist will also be able to detect small adhesions and work them
+      out before becoming a serious issue. Improves Posture When it comes to golf, good posture is
+      critical for better performance. Golf and fitness massage therapy can reduce the muscle
+      tightness that causes postural imbalances, helping the body assume correct position. Improved
+      posture will not only improve your golf swing but also reduce the risk of elbow injuries –like
+      the dreaded golf elbow - and tendonitis. Increases Range of Motion & Flexibility. Golfers
+      often experience tightness in the forearms, shoulders, lower back, and hips due to overuse of
+      playing. Golf massage is effective at loosening and stretching stiff joints and muscles, thus
+      creating a more fluid motion in the golf swing. Regular massage, combined with daily
+      stretching, will improve your range of motion and flexibility as well as increase your power
+      and performance. GOLF MASSAGE CONCENTRATES ON RELAXING THE BACK, HAMSTRINGS, HIP FLEXORS,
+      SHOULDERS, AND WRISTS TO CREATE A BIGGER SHOULDER TURN, BETTER HIP AND BACK FLEXIBILITY, AND A
+      MORE POWERFUL TORQUE AND HIGHER FINISH. Pain Relief. It should come as no surprise, but the
+      force and repetitive movement used in a golf swing can result in muscle soreness and spasms –
+      both of which can be extremely painful. Deep tissue massage therapy can help lengthen and
+      relax the contracted muscle and provide much-needed relief to soreness. Improves Circulation.
+      Another great benefit to golf massage is its ability to improve blood flow around the body.
+      Good circulation helps prevent the buildup of toxins in the muscles, which left untreated, can
+      cause muscle pain and stiffness. The rhythmic strokes and pressure used in massage releases
+      the accumulation of toxins and helps oxygenated blood get to your sore muscles, allowing them
+      to recover faster. Improves Mental Focus. In every sport, mental toughness always plays a
+      role. When that key putt is on the line, too much stress, anxiety, and pressure can negatively
+      affect your game and cause you to lose focus. Stress can also result in the involuntary
+      clenching of muscles, which is counterproductive for golfers who need their swing to be as
+      fluid as possible. Massage therapy is a great way to relax your mind, thus helping your
+      muscles and improving your overall game. IS THERE A TIME I SHOULDN’T GET A GOLF MASSAGE? Great
+      question! Our South Tampa massage therapists generally recommend you book your golf massage at
+      least 48 hours prior to tee time. If you book an appointment too close to playing, you could
+      be too sore to play, or your muscles won’t perform at their best. Close to tee time? Try a
+      standing forward bend stretch. Golfers can try this stretch before hitting the links, putting
+      green, or in between holes as needed. Here’s how to make the most of the stretch: Standing up
+      straight (chin up!) feet together with your shoulders relaxed. Reach arms behind your back and
+      interlock fingers. Lift your shoulders towards your ears and lift your hands away from the
+      back. Slowly bend forward at the waist. Your back should be flat, not rounded. As far as your
+      body allows, bend forward and lift your hands above your head as far forward as comfortable.
+      Hold for 10-20 seconds and release. Repeat two to three times. When in a full standing forward
+      bend stretch, you may feel slight tension in your hamstrings and/or in your shoulders. If the
+      tension is too much, reduce the angle of your back. Don’t extend beyond your ability. With
+      proper form, you can release tension in your back and shoulders so you can get back in
+      alignment for your golf strokes. MASSAGE FOR GOLFERS As you can see, there are many benefits
+      of golf massage. Feel the difference that golf and fitness massage at Tampa Bay Sports &
+      Medical Massage can make for your game! To speak with a specialist, call us at +(351) 963 531
+      684. We’re happy to answer your massage questions. Of course, questions are welcome during
+      your session too; Book your appointment for a golf sports massage today!
+    </p>
+  </div>
+);
---
+++ src/markdown-pages/post-5.md
@@ -0,0 +1,78 @@
+---
+slug: '/blog/health-therapeutic-massage'
+date: 2021-08-08
+title: Medical massage can be tailored to your health needs and help you recover faster.
+tags:
+  - back pain
+  - chronic pain
+  - massage
+  - massage therapy
+  - medical massage
+  - mental health
+  - neck pain
+  - stress
+  - therapeutic massage
+featured: therapeutic-massage-therapy.jpg
+excerpt: Massage is best known as a way to help you relax and relieve stress.  A gentle soothing massage can reduce stress, relieve muscle aches, improve your sleep and improve your mood.
+---
+
+# Why You Should Try Medical or Therapeutic Massage
+
+Massage is best known as a way to help you relax and relieve stress. A gentle soothing massage can reduce stress, relieve muscle aches, improve your sleep and improve your mood.
+
+## Medical massage can be tailored to your health needs and help you recover faster
+
+Lesser known is the fact that massage also has benefits far beyond mere relaxation. Licensed massage therapist [Danylo Hips](https://www.algarvehomemassageandbeauty.com/about-us/#staffDanylo), says that medical, or therapeutic, massage can help you with more serious medical issues, too.
+
+Massage therapy is given by licensed professionals who are trained to find and focus on problem areas.
+
+This therapeutic type of massage involves more focused work on your body’s soft tissue — the muscles, tendons and ligaments that move and support your body.
+
+Your physician, chiropractor, or physical therapist might recommend that you receive a therapeutic massage to complement other therapies you’re already receiving to treat a particular area of your body.
+
+Or, you may not be getting the results you want from other traditional treatments. Receiving massage therapy on a regular basis may help in this case, too.
+
+## It can ease discomfort and pain
+
+A massage therapist can work on a number of medical issues that are either chronic (meaning they last a long time) or acute (a medical issue that has happened recently and can be cared for in the near future.)
+
+These issues can include:
+
+- Repetitive stress injuries from sitting or standing postures that are held for several hours a day.
+- Migraines, cluster headaches, tension headaches or sinus headaches.
+- Whiplash.
+- Temporomandibular joint and muscle disorder.
+- Strains and sprains (after inflammation has gone down).
+- Low back pain.
+- Tendonitis.
+- Radiating pain.
+- Post-surgical scar tissue (with doctor’s approval).
+- Frozen shoulder.
+
+You can rename the current file by clicking the file name in the navigation bar or by clicking the **Rename** button in the file explorer.
+
+## What to expect at a massage therapy visit
+
+“There are no typical therapeutic massages routines,” Danylo says. “Your massage therapist will tailor the massage to your needs, depending on the areas of issue.”
+
+When you first call to make your appointment, it’s important to explain to the massage therapist exactly what you’d like worked on. Talk to the therapist about what your specific needs are. For example, why did your doctor recommend you have a therapeutic massage? What are the areas of concern, and why or how did it happen?
+
+“The more info you provide, the better,” he says. And if the massage therapist is unfamiliar with your issues, you may be referred to a colleague who can better serve your specific needs.
+
+During our visit, your therapist may have soft lighting and music playing, just like with a relaxation massage. Instead of a whole-body treatment though, a therapeutic massage session tends to focus on your targeted area of concern — for an extended period of time.
+
+If you schedule a 90-minute massage (this is a longer session), the massage therapist will dedicate most of the time to your areas of concern, but may also include other muscle groups to support a more comprehensive outcome.
+
+## Spot work as part of an extended program
+
+Another way medical massage might be given is if massage therapy is recommended as an extended part of your therapeutic treatment at your chiropractor’s office or physical therapy clinic. In this case, you’ll probably receive something called “spot work.”
+
+The massage therapist will work on your area of concern for a shorter period of time — about 30 minutes. Spot work complements the treatment you’re already given by the other practitioners.
+
+Your massage therapist may also incorporate stretches into the treatment plan. If that’s the case, Danylo recommends wearing loose-fitting clothing or active-wear to the appointment.
+
+“A therapeutic massage can greatly benefit your overall well-being,” Danylo emphasizes. “Focused massage techniques can help decrease pain and increase your range of motion, make it easier to move and function more normally, and help you heal faster from a variety of medical concerns. If it’s an option in your treatment, the outcomes can be very positive.”
+
+To speak with a specialist, call us at **+(351) 963 531 684**. We’re happy to answer your [massage questions](https://www.algarvehomemassageandbeauty.com/prices-massage). Of course, questions are welcome during your session too;
+
+### Book your appointment for a golf sports massage today!.
